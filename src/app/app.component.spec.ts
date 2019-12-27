1f182d4cf1f21df593d6563156bbe9b1c9b3f192
DenisuDevForFood <denisfeier98@yahoo.com>
2019-12-28 01:23:25 +0200

    added all

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'musicPlayer'`, () => {
+  it(`should have as title 'projectPdss'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('musicPlayer');
+    expect(app.title).toEqual('projectPdss');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('musicPlayer app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('projectPdss app is running!');
   });
 });
