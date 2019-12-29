c3564afa8df564570a499b66dc5935ca68f1aecb
Daiana22 <daianagroza@yahoo.com>
2019-12-29 20:16:31 +0200

    new commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,11 +10,13 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
+import { SigninComponent } from './signin/signin.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MainNavComponent
+    MainNavComponent,
+    SigninComponent
   ],
   imports: [
     BrowserModule,
